dbb2354ca9489c6e72e3fd4882f2623aeaf87b96
sioodmy <hello@sioodmy.dev>
2024-05-31 13:12:50 +0200

    feat: add overrides

--- seashell/default.nix
+++ seashell/default.nix
@@ -17,18 +17,21 @@
     pkgs.lib.concatStringsSep "\n"
     (pkgs.lib.mapAttrsToList (k: v: "alias ${k} = ${v}") aliases);
 in
-  inputs.wrapper-manager.lib.build {
+  (inputs.wrapper-manager.lib.build {
     inherit pkgs;
     modules = [
       {
         wrappers =
           {
             seashell = {
-              basePackage = pkgs.nushell;
+              basePackage = pkgs.nushellFull;
               pathAdd = packages;
               flags = [
                 "--config"
                 (pkgs.writeText "config.nu" (nuconfig + aliasesStr))
+                # we don't really need it
+                "--env-config"
+                "/dev/null"
               ];
               env.STARSHIP_CONFIG.value = toml.generate "starship.toml" starship-settings;
               renames = {
@@ -39,4 +42,8 @@ in
           // configs;
       }
     ];
-  }
+  }).overrideAttrs (_: prev: {
+    pname = "seashell";
+    meta.mainProgram = "seashell";
+    passthru.shellPath = "/bin/seashell";
+  })
