14b9576282733564c8b7c1bc194d502d8a37a84c
sioodmy <hello@sioodmy.dev>
2024-06-25 18:05:32 +0200

    feat: idle inhibit

--- andromeda-niri/config.nix
+++ andromeda-niri/config.nix
@@ -242,6 +242,8 @@ in
       (leaf "spawn-at-startup" ["ags-wrapped"])
       (leaf "spawn-at-startup" ["${e pkgs.swaybg}" "-i" "${./wall.jpg}"])
       (leaf "spawn-at-startup" ["${e pkgs.wlsunset}" "-l" "50.0" "-L" "19.94"])
+      (leaf "spawn-at-startup" ["${e pkgs.sway-audio-idle-inhibit}"])
+
       (leaf "spawn-at-startup" ["swayidle"])
       (leaf "screenshot-path" "~/pics/ss/ss%Y-%m-%d %H-%M-%S.png")
       (flag "environment")
